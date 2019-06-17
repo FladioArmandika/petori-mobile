d6fc2a9ac74226c59d804bde127f3e5116768a9d
fladioarmandika <91378531fladio>
2019-06-17 19:20:01 +0700

    dwadawd

---
+++ app/src/main/java/io/aigb/petori/views/LoginActivity.java
@@ -0,0 +1,15 @@
+package io.aigb.petori.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.aigb.petori.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
